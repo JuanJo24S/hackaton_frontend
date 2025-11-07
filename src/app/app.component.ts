d6c182c05883f253f02610682eb4b4c0bba38daa
JuanJo24S <juanjosecodes24@gmail.com>
2025-11-07 14:49:11 -0500

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonApp, IonRouterOutlet, IonHeader, IonContent, IonFooter, IonToolbar } from '@ionic/angular/standalone';
 import { NavPage } from "./nav/nav.page";
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonRouterOutlet, IonHeader, IonToolbar, IonContent, NavPage],
+  imports: [IonRouterOutlet, IonHeader, IonToolbar, NavPage, IonContent],
 })
 export class AppComponent {
   constructor() {}
